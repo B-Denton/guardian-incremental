c31706badfc11513375277f260fd3078364430a8
B-Denton <84455589+BSDenton@users.noreply.github.com>
2023-08-18 16:12:06 +0100

    Fixed Dragon Levelling not increasing stats

--- src/data/dragon-data.js
+++ src/data/dragon-data.js
@@ -58,8 +58,7 @@ class Dragon {
         this.xp = 0;
         this.level++;
         $.each(['hp', 'attack', 'defense', 'speed'], ( _ , item) => {
-            console.log (Math.round(((this[item].max * 1.1) * 100) / 100))
-            this[item].max = Math.round(((this[item].max * 1.1) * 100) / 100) ;
+            this[item].max = Math.ceil(((this[item].max * 1.1) * 100) / 100) ;
             this[item].value = this[item].max;
         })
     };
